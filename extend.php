ee7ee035fff7f58ab6e441c1b0d3b92c65b5f855
Taylor Lottner <50132994+TaylorLottner@users.noreply.github.com>
2022-08-18 20:00:20 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -8,16 +8,16 @@ return [
     (new Extend\Frontend('forum'))
         ->content(function (Document $document) {
             $document->head[] = '
-            <link href="https://cdn.jsdelivr.net/gh/moerio/Mixture@latest/APlayer.min.css">
-            <script src="https://cdn.jsdelivr.net/gh/moerio/Mixture@latest/APlayer.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/meting@2.0.1/dist/Meting.min.js"></script>
+            <link rel="stylesheet" href="https://fastly.jsdelivr.net/gh/moerio/Mixture@latest/APlayer.min.css">
+            <script src="https://fastly.jsdelivr.net/gh/moerio/Mixture@latest/APlayer.min.js"></script>
+            <script src="https://fastly.jsdelivr.net/npm/meting@2.0.1/dist/Meting.min.js"></script>
             ';
         }),
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
             $config->BBCodes->addCustom(
-                '[automusic src="{TEXT1?}" mini="{URL1?}" autoplay="{URL2?}" loop="{URL3?}" volume="{URL4?}"][/automusic]',
-                '<div><meting-js auto="{TEXT1}" mini="{URL1}" autoplay="{URL2}" loop="{URL3}" volume="{URL4}"></meting-js></div>'
+                '[automusic src="{TEXT1?}" mini="{URL1?}" autoplay="{URL2?}" loop="{URL3?}" volume="{URL4?}" order="{URL5?}"][/automusic]',
+                '<div><meting-js auto="{TEXT1}" mini="{URL1}" autoplay="{URL2}" loop="{URL3}" volume="{URL4}" order="{URL5}"></meting-js></div>'
             );
             $config->BBCodes->addCustom(
                 '[cloudmusic server="{TEXT1?}" type="{URL5?}" url="{URL6?}" mini="{URL1?}" autoplay="{URL2?}" loop="{URL3?}" volume="{URL4?}" cover="{URL7?}"][/automusic]',
@@ -29,7 +29,7 @@ return [
             );
 	        $config->BBCodes->addCustom(
                 '[video url="{URL1?}" width="{URL2?}" height="{URL3?}"][/video]',
-                '<iframe src="{URL1}" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen" width="{URL2}" height="{URL3}" frameborder="0"></iframe>'
+                '<iframe src="https://m3u8.fairysen.com/?url={URL1}" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen" width="{URL2}" height="{URL3}" frameborder="0"></iframe>'
             );
         })
 ];
